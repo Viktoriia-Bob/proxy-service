481f9c94465517828e02101d93691b6dd7b65d75
VikaBob <vikabobir32@gmail.com>
2024-09-02 13:10:07 +0300

    add validation pipe

--- apps/proxy-service/src/main.ts
+++ apps/proxy-service/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 
   app.connectMicroservice<MicroserviceOptions>({
